c96ffefdb9b5c1371416c560b67a6bae64950f2e
hello20110562 <hello20110562@gmail.com>
2014-12-02 19:12:33 +0800

    java练习
    vartest

---
+++ ImageViewer.java
@@ -0,0 +1,12 @@
+//import java.awt.EventQueue;
+//import java.awt.event.
+public class ImageViewer{
+	public static void main(String args[]){
+	    EventQueue.invokeLater(new Runnable()){
+			public void run(){
+				JFrame frame = new ImageViewerFrame();
+				frame.setDefaultClodeOperation(JFrame.EXIT_ON_CLOSE;
+			}
+			}
+		}
+	}
